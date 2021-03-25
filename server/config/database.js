33cce75f72035d200ff0c3000eebcc922f94fc67
James Middleton <hello@jamesmiddleton.me>
2021-03-25 20:27:30 +0000

    strapi: Use mongodb instead of pg

--- server/config/database.js
+++ server/config/database.js
@@ -1,29 +1,11 @@
-module.exports = ({ env }) => {
-  const connectionSettings =
-    env('NODE_ENV') === 'development'
-      ? {
-          client: 'sqlite',
-          filename: '.tmp/data.db',
-        }
-      : {
-          client: 'postgres',
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          username: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'postgres'),
-        }
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: connectionSettings,
-        options: {
-          useNullAsDefault: true,
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI', 'mongodb://localhost/strapi'),
       },
     },
-  }
-}
+  },
+})
